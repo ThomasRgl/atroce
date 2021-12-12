fe64c67a9e7cebfcfc2f36319baa49fbc8f6702f
ThomasRgl <thomas.roglin@free.fr>
2021-12-12 23:46:15 +0100

    ajout back end

--- src/baselib.ml
+++ src/baselib.ml
@@ -1,4 +1,5 @@
 open Ast
+open Mips
 
 module Env = Map.Make(String)
 
@@ -19,3 +20,37 @@ let _types_ =
             ; "%div", Func_t (Int_t, [ Int_t ; Int_t ])
        
         ])
+
+
+let builtins =
+  [ Label "_add"
+  ; Lw (T0, Mem (SP, 0))
+  ; Lw (T1, Mem (SP, 4))
+  ; Add (V0, T0, T1)
+  ; Jr RA
+      
+  ; Label "_mul"
+  ; Lw (T0, Mem (SP, 0))
+  ; Lw (T1, Mem (SP, 4))
+  ; Mul (V0, T0, T1)
+  ; Jr RA
+
+  ; Label "puti"
+  ; Lw (A0, Mem (SP, 0))
+  ; Li (V0, Syscall.print_int)
+  ; Syscall
+  ; Jr RA
+
+  ; Label "geti"
+  ; Lw (A0, Mem (SP, 0))
+  ; Li (V0, Syscall.read_int)
+  ; Syscall
+  ; Jr RA
+
+  ; Label "puts"
+  ; Lw (A0, Mem (SP, 0))
+  ; Li (V0, Syscall.print_str)
+  ; Syscall
+  ; Jr RA
+
+  ]
